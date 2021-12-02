86d5924910b8186b0bc7dc9f3ba58c9f8155b57a
Ferchus <zap408@amerike.edu.mx>
2021-12-02 11:56:52 -0600

    Added Comments

--- ZAP408_Proyecto1_HistoriaNarrada/Source.cpp
+++ ZAP408_Proyecto1_HistoriaNarrada/Source.cpp
@@ -6,37 +6,40 @@
 #include <dialogos.h> //Aqu[i se encuentran todos los diálogos
 #include <limits> // Permitirá validar los valores // No lo terminé usando, pero usualmente necesitas incluirlo para std::cin.ignore();
 
+// Kid A Mnesia.wav = Música de fondo
+// sans.wav = Sonido de mostrar letras
+
 	void Dialogo(std::string str) // Muestra los dialogos letra por letra con la voz de sans
 	{
-		std::system("CLS");
+		std::system("CLS"); //Limpia la consola
 
-		int sansSpeed = 1; //Dicta la velocidad con la que se muestran las letras
+		int sansSpeed = 1; //Dicta la el retraso entre cada letra mostrada
 
-		PlaySound(NULL, 0, 0);
+		PlaySound(NULL, 0, 0); //Detiene la música
 		Sleep(1000);
 
 		int numeroLetras = 0;
 		std::string dialogo = str;
 		
-		while (dialogo[numeroLetras] != '\0')
+		while (dialogo[numeroLetras] != '\0') //Este bucle muestra letra por letra hasta que se encuentra con '\0', el cual marca el final del string
 		{
 			std::cout << str[numeroLetras];
-			if (numeroLetras % 3 == 0)
+			if (numeroLetras % 3 == 0) //Hace que solo se escuche el sonido cada tres letras, pues si no, iría muy rápido
 			{
-				PlaySound(NULL, 0, 0);
-				PlaySound(TEXT("sans.wav"), NULL, SND_ASYNC);
+				PlaySound(NULL, 0, 0); //Detiene el sonido de la letra
+				PlaySound(TEXT("sans.wav"), NULL, SND_ASYNC); //Reproduce el sonido de la letra
 			}
-			Sleep(sansSpeed);	
+			Sleep(sansSpeed); //
 			numeroLetras++;
 		}	
 
 		Sleep(1500);
 		std::cout << std::endl << std::endl;
 
-		PlaySound(TEXT("Kid A Mnesia.wav"), NULL, SND_LOOP | SND_ASYNC);
+		PlaySound(TEXT("Kid A Mnesia.wav"), NULL, SND_LOOP | SND_ASYNC); //Reanuda la música de fondo
 	}
 
-	int UnoDos() //Le pide al usuario introducir uno o dos
+	int UnoDos() //Le pide al usuario introducir uno o dos y hace la comprobación de valores. 
 	{
 		// Boleano para comprobar que el valor sea válido
 
@@ -44,17 +47,17 @@
 		do {
 			std::cout << "\nIntroduce tu respuesta: ";
 			std::cin >> x;
-			if (x != 1 && x != 2)
+			if (x != 1 && x != 2) // Comprobación
 			{
 				std::cout << "Número inválido. Intenta otra vez." << std::endl;
 				std::cin.clear();
-				std::cin.ignore(9223372036854775807, '\n'); //Por alguna razón no encontró "std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');" Válido, entonces busqué el valor de "std::numeric_limits<std::streamsize>::max()", que me dió "9223372036854775807", así que lo remplacé con eso.
+				std::cin.ignore(9223372036854775807, '\n'); //Por alguna razón no encontró "std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');" Válido, entonces busqué el valor de "std::numeric_limits<std::streamsize>::max()", que me dió "9223372036854775807", así que lo remplacé con eso. Sirve para limpiar el buffer
 			}
 		} while (x != 1 && x != 2);
-		return x;
-	};
+		return x; // Regresa el número introducido
+	}; 
 
-	void Ending(std::string str)
+	void Ending(std::string str) //Muestra el nombre del ending que te tocó, los créditos y las notas del juego
 	{
 		std::cout << "Presiona ENTER para continuar" << std::endl;
 
@@ -63,9 +66,13 @@
 
 		std::system("CLS");
 
-		std::string endingText = str;
+		std::string endingText = str; //Toma el texto de los parámetros
 
-		std::cout << endingText << std::endl;
+		std::cout << endingText << std::endl << std::endl;
+
+		std::cout << "Código por Fernando González (Ferchus)" << std::endl;
+		std::cout << "Creado como proyecto final para la materia Fundamentos de Programación en el Instituto Universitario Amerike" << std::endl;
+
 	}
 
 
@@ -79,6 +86,9 @@ int main()
 	std::cout << "Bienvenido a mi juego.\n\nTendrás que tomar decisiones presionando los números que aparecerán en la pantalla y confirmando con la tecla ENTER.\n\nEspero que lo disfrutes.\n\nPresiona ENTER para comenzar\n";
 	std::cin.clear();
 	std::cin.ignore(9223372036854775807, '\n'); //También lo usamos aquí para limpiar el buffer antes de empezar con el juego.
+
+	// Vea "ZAP408_Diagrama Proyecto 1.pdf" para entender el flujo de decisiones
+	// Empiezan las decisiones
 
 	Dialogo(intro);
 	std::cout << "1. Contestas el celular." << std::endl;
@@ -195,7 +205,7 @@ int main()
 	}
 
 
-	//Branching 
+	//Plantilla que usé para el branching
 	/*
 	if (UnoDos() == 1)
 		{
